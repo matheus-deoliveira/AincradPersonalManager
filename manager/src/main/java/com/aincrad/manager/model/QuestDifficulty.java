b6a4ae2fcfa37ae999098735087907386e03c4ec
Matheus de Oliveira <matheusdeoliveiraes@gmail.com>
2025-11-23 17:13:11 -0300

    feat: database entities created (Issue #2)

---
+++ manager/src/main/java/com/aincrad/manager/model/QuestDifficulty.java
@@ -0,0 +1,10 @@
+package com.aincrad.manager.model;
+
+public enum QuestDifficulty {
+    E_RANK, // tarefas fáceis
+    D_RANK,
+    C_RANK,
+    B_RANK,
+    A_RANK,
+    S_RANK  // tarefas muito difíceis
+}
